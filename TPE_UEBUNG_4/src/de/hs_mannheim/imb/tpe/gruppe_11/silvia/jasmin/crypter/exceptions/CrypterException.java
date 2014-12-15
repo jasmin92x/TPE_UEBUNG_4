16787f5afee164f35b456bc1eb629ffffb57135b
jasmin92x <j.cano@gmx.de>
2014-12-15 18:25:08 +0100

    Klassen CrypterExeption, IllegalKeyException

---
+++ TPE_UEBUNG_4/src/de/hs_mannheim/imb/tpe/gruppe_11/silvia/jasmin/crypter/exceptions/CrypterException.java
@@ -0,0 +1,13 @@
+package de.hs_mannheim.imb.tpe.gruppe_11.silvia.jasmin.crypter.exceptions;
+/**
+ * 
+ * @author Jasmin Cano, Silvia Yildiz
+ *
+ */
+public class CrypterException extends Exception {
+
+	public CrypterException(String message) {
+		super(message);
+	}
+
+}
---
+++ TPE_UEBUNG_4/src/de/hs_mannheim/imb/tpe/gruppe_11/silvia/jasmin/crypter/exceptions/IllegalKeyException.java
@@ -0,0 +1,13 @@
+package de.hs_mannheim.imb.tpe.gruppe_11.silvia.jasmin.crypter.exceptions;
+/**
+ * 
+ * @author Jasmin Cano, Silvia Yildiz
+ *
+ */
+public class IllegalKeyException extends CrypterException {
+
+	public IllegalKeyException(String message) {
+		super(message);
+	}
+
+}
